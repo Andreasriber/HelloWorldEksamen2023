b38e3034edd7d2c8cf448df5a138417249817fb3
Andreasriber <andreasriber@gmail.com>
2023-11-22 14:32:55 +0100

    San Fran

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World this is Bangalore!')
+  res.send('Hello World this is San Fransisco baby!')
 })
 
 app.listen(port, () => {
